622932cabfc81e8732cf7c7a8178c1b647b798df
Hatice <haticekaraty@gmail.com>
2021-02-20 20:35:04 -0500

    CLI class call, main_menu, user_input methods

--- lib/plant_species/cli.rb
+++ lib/plant_species/cli.rb
@@ -1,3 +1,31 @@
 class CLI
+    def self.call
+        API.create_plant
+        main_menu
+    end
+
+    def self.main_menu
+        puts "Hello! Welcome to plant classification viewer! Below, you'll see a list of plant species you can learn more about them"
+        Plant.list_plants
+        get_user_input
+    end
+
+    def self.get_user_input
+        puts "Please select a plant from the list by typing the corresponding number only!"
+        user_input = gets.chomp
+        @@user_index = user_input.to_i
+        self.error
+    end 
+
+    def self.error
+        range = Plant.all.count
+        if (0..range).include?(@@user_index) 
+            puts "displaying details"
+        else
+            puts "I don't get that. Please enter a number from the list or type 'exit' to quit."
+        end
+    end
+   
+
 
 end
--- lib/plant_species/plant.rb
+++ lib/plant_species/plant.rb
@@ -2,15 +2,15 @@ class Plant
 
     @@all = []
 
-    attr_accessor :common_name , :scientific_name , :year, :genus, :family
+    attr_accessor :common_name , :scientific_name , :year, :genus, :family, :kingdom
 
-    def initialize(common_name = nil, scientific_name = nil, year = nil, genus = nil, family= nil)
+    def initialize(common_name = nil, scientific_name = nil, year = nil, genus = nil, family= nil , kingdom = "Plantae")
         @common_name = common_name
         @scientific_name = scientific_name
         @year = year
         @genus = genus
         @family = family
-    
+        @kingdom = kingdom
         save
     end
 
@@ -27,5 +27,5 @@ class Plant
             puts "#{index}. #{plant_object.common_name}"
         end
     end
-        
+    
 end
