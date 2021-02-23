7eeeab359cbaf486cfaf0e34391bc11061d83452
Hatice <haticekaraty@gmail.com>
2021-02-22 20:05:35 -0500

    Plant instances save methods and class get method

--- lib/plant_species/plant.rb
+++ lib/plant_species/plant.rb
@@ -1,5 +1,5 @@
 class Plant
-
+    @@all = []
     attr_accessor :common_name , :scientific_name , :year, :genus, :family, :kingdom
 
     def initialize(common_name = nil, scientific_name = nil, year = nil, genus = nil, family= nil , kingdom = "Plantae")
@@ -9,6 +9,15 @@ class Plant
         @genus = genus
         @family = family
         @kingdom = kingdom
-        
+        save
     end
+
+    def save 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
 end
